7204bb6d8cddfae7fb80213783c2be08a2b315f4
n-bhasin <nkbhasin28@gmail.com>
2024-09-19 09:06:07 -0400

    created a custome hook, critic score and optimized images

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,15 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+const CriticScore = ({ score }: Props) => {
+  const color = score > 75 ? "green" : score > 60 ? "yello" : "";
+  return (
+    <Badge colorScheme={color} borderRadius="4px" paddingX={2} fontSize="14px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
