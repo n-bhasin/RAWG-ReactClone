0bc3b3f5debe7750309042b94a8a4e78b92906b7
n-bhasin <nkbhasin28@gmail.com>
2024-09-25 08:47:30 -0400

    sort games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -18,7 +18,11 @@ const useGames = (gameQuery: GameQuery) =>
   useData<Game>(
     "/games",
     {
-      params: { genres: gameQuery.genre?.id, platform: gameQuery.platform?.id },
+      params: {
+        genres: gameQuery.genre?.id,
+        platform: gameQuery.platform?.id,
+        ordering: gameQuery.sortOrder,
+      },
     },
     [gameQuery]
   );
